2f1655586d490022cfa43066821c3dbeccedc567
Madelyn Romero <m.romero013@gmail.com>
2019-10-23 09:17:21 -0600

    Add working code for entire patron class and patron test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -16,4 +16,15 @@ class PatronClass < Minitest::Test
     assert_equal "Bob", @patron.name
     assert_equal 20, @patron.spending_money
   end
+
+  def test_it_begins_with_empty_array_for_interests
+    assert_equal [], @patron.interests
+  end
+
+  def test_it_can_add_interests
+    @patron.add_interest("Dead Sea Scrolls")
+    @patron.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron.interests
+  end
 end
